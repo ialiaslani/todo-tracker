3f6de52b2a1261a6a83bfb1ce159028aab606af2
Ali <aliaslani.mm@gmail.com>
2022-01-17 15:10:22 +0330

    Add Field Component

---
+++ src/Components/TodoTracker/Field.js
@@ -0,0 +1,9 @@
+
+const Field = ({children}) => {
+
+    return <div className='container' children={children} />
+
+}
+
+
+export default Field
--- src/Components/TodoTracker/Tasks.js
+++ src/Components/TodoTracker/Tasks.js
@@ -3,6 +3,7 @@ import {useState,useEffect} from "react";
 import BackendMiddleware from "../../Services/BackendMiddleWare";
 import api from "../../Services/api/Task";
 import AddTask from "./AddTask";
+import Field from "./Field";
 
 const Tasks = () => {
 
@@ -46,7 +47,7 @@ const Tasks = () => {
     return (
         <div>
             <AddTask onAdd={addTask} />
-            <>
+            <Field>
                 {
                     tasks.length > 0 ? tasks.map(({_id, description}) => (
                         <Task
@@ -57,7 +58,7 @@ const Tasks = () => {
                         />
                     )) : 'No Tasks To Show'
                 }
-            </>
+            </Field>
         </div>
     )
 }
